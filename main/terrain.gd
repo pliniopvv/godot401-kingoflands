ec34d70dbc433c2f0451fcbfdf7c7817e1cefb3c
gangss <gangss2@hotmail.com>
2023-09-25 14:57:22 -0300

    Geration of chunks otimized

--- main/terrain.gd
+++ main/terrain.gd
@@ -7,7 +7,7 @@ extends Node3D
 @export_category("Cfg das chunks individuais")
 @export var size_chunks = Vector2(12,12)
 @export var altura_chunks = 5
-@export var lod = 5
+@export var meshResolution = 5
 @export var flat = false
 
 @onready var chunk = preload("res://nodes/terrain/chunk.tscn")
@@ -17,6 +17,7 @@ var pos = Vector2(0,0)
 var player: CharacterBody3D
 var surfacetool: SurfaceTool
 var noise: FastNoiseLite
+var bnoise: FastNoiseLite
 
 func set_player(p: CharacterBody3D):
 	player = p
@@ -26,6 +27,13 @@ func _ready():
 		noise = FastNoiseLite.new()
 		noise.frequency = 0.07
 		noise.noise_type = FastNoiseLite.TYPE_PERLIN
+		
+		bnoise = FastNoiseLite.new()
+		bnoise.frequency = 0.07
+		bnoise.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
+		bnoise.fractal_gain = 0.2
+		bnoise.fractal_octaves = 8
+		
 		generate_chunks(pos)
 
 func generate_chunks(pos: Vector2):
@@ -37,7 +45,7 @@ func generate_chunks(pos: Vector2):
 			var pC = Vector2(fx,fz)
 			if not map.has(pC):
 				#criação das chunks
-				var chunk = generate_chunk(noise, surfacetool, pC)
+				var chunk = generate_chunk(bnoise, noise, surfacetool, pC)
 				map[pC] = chunk
 				add_child(chunk)
 
@@ -47,11 +55,11 @@ func _process(delta):
 		pos = chunk_pos
 		generate_chunks(pos)
 
-func generate_chunk(noise:FastNoiseLite, surfacetool: SurfaceTool, posChunk: Vector2):
+func generate_chunk(bnoise:FastNoiseLite, noise:FastNoiseLite, surfacetool: SurfaceTool, posChunk: Vector2):
 	var c = chunk.instantiate()
 	c.size = size_chunks
 	c.altura = altura_chunks
-	c.lod = lod
+	c.resolution = meshResolution
 	c.flat = flat
-	c.GenerateTerrain(noise, surfacetool, posChunk)
+	c.generate_terrain(bnoise, noise, surfacetool, posChunk)
 	return c
--- nodes/terrain/chunk.gd
+++ nodes/terrain/chunk.gd
@@ -8,11 +8,12 @@ class_name GenericChunk
 @export var humidade: NoiseTexture2D
 @export_group("Valores iniciais")
 @export var size = Vector2(12.0,12.0)
-@export var lod = 5
+@export var resolution = 5
 @export var altura = 5.0
 @export var flat = false
 
 var center_terrain = size/2
+var center_offset = 0.5
 
 var uvx = 0
 var uvz = 0
@@ -20,37 +21,33 @@ var uvz = 0
 func _ready():
 	pass
 
-func GenerateTerrain(noise: FastNoiseLite, st:SurfaceTool, posOrigin:Vector2):
-	for _z in range(lod+1):
-		if uvz == 0: uvz = 1
-		else: uvz = 0
-		for _x in range(lod+1):
-			var unid = size/lod
-			var unidPos = Vector2(_x, _z) * unid
-			var pontoNaMesh = posOrigin *size +unidPos -center_terrain #Vector2(posOrigin.x*size.x+(unidPos.x-center_terrain.x), posOrigin.y*size.y+(unidPos.y-center_terrain.y))
-			
-			var y = 0
+func generate_terrain(bnoise: FastNoiseLite, noise: FastNoiseLite, st:SurfaceTool, posOrigin:Vector2):
+	var mSize = Vector3(size.x, 1.0, size.y)
+	position = Vector3(posOrigin.x, 0, posOrigin.y) * mSize
+	for _z in range(resolution+1):
+		for _x in range(resolution+1):
+			var percent = Vector2(_x,_z)/resolution
+			var pontoNaMesh = Vector3((percent.x-center_offset), 0, (percent.y-center_offset))
 			if not flat:
-				y = noise.get_noise_2d(pontoNaMesh.x, pontoNaMesh.y) * altura
+				pontoNaMesh.y = noise.get_noise_2d(posOrigin.x+pontoNaMesh.x, posOrigin.y+pontoNaMesh.z) * altura * resolution
+			var vertex = pontoNaMesh * mSize
 			
-			if uvx == 0: uvx = 1
-			else: uvx = 0
-			#var uv = Vector2(uvx, uvz)
-			var uv = unidPos
+			var uv = Vector2(0,0)
+			uv.x = percent.x
+			uv.y = percent.y
+			
 			st.set_uv(uv)
-			
-			var vertex = Vector3(pontoNaMesh.x, y, pontoNaMesh.y)
 			st.add_vertex(vertex)
 	
 	var vert = 0
-	for z in range(lod):
-		for x in range(lod):
+	for z in range(resolution):
+		for x in range(resolution):
 			st.add_index(vert+0)
 			st.add_index(vert+1)
-			st.add_index(vert+lod+1)
-			st.add_index(vert+lod+1)
+			st.add_index(vert+resolution+1)
+			st.add_index(vert+resolution+1)
 			st.add_index(vert+1)
-			st.add_index(vert+lod+2)
+			st.add_index(vert+resolution+2)
 			vert+=1
 		vert+=1
 	
@@ -63,28 +60,42 @@ func GenerateTerrain(noise: FastNoiseLite, st:SurfaceTool, posOrigin:Vector2):
 	
 	var sm = ShaderMaterial.new()
 	sm.shader = preload("res://nodes/terrain/chunk.gdshader")
-	sm.set_shader_parameter("grama", grama)
-	sm.set_shader_parameter("deserto", deserto)
-	
-	var fn = FastNoiseLite.new()
-	fn.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
-	fn.frequency = 0.0001
-	fn.fractal_gain = 0.8
-	fn.fractal_octaves = 8
-	var offset = Vector3(posOrigin.x*size.x-center_terrain.x,posOrigin.y*size.y-center_terrain.y,0.0)
-	#var offset = Vector3(posOrigin.x*size.x,posOrigin.y*size.y,0.0)
-	print(offset)
-	fn.offset = offset
-	
-	humidade = NoiseTexture2D.new()
-	humidade.width = size.x
-	humidade.height = size.y
-	humidade.noise = fn
-	humidade.normalize = false
-
-	sm.set_shader_parameter("humidade", humidade)
 	sm.set_shader_parameter("origin", posOrigin)
 	sm.set_shader_parameter("size", size)
+	
+	#
+	#				CONFIGURANDO BIOMAS
+	#
+	
+	var oeste = grama
+	var leste = grama
+	var norte = grama
+	var sul = grama
+	var biome = grama
+	
+	var hum = bnoise.get_noise_2dv(posOrigin)
+	var hOeste = bnoise.get_noise_2d(posOrigin.x-1, posOrigin.y)
+	var hLeste = bnoise.get_noise_2d(posOrigin.x+1, posOrigin.y)
+	var hNorte = bnoise.get_noise_2d(posOrigin.x, posOrigin.y+1)
+	var hSul = bnoise.get_noise_2d(posOrigin.x, posOrigin.y-1)
+	
+	if (hum < 0.35):
+		biome = deserto
+	if (hOeste < 0.35):
+		oeste = deserto
+	if (hLeste < 0.35):
+		leste = deserto
+	if (hNorte < 0.35):
+		norte = deserto
+	if (hSul < 0.35):
+		sul = deserto
+	
+	sm.set_shader_parameter("biome", biome)
+	sm.set_shader_parameter("oeste", oeste)
+	sm.set_shader_parameter("leste", leste)
+	sm.set_shader_parameter("norte", norte)
+	sm.set_shader_parameter("sul", sul)
+	
 	mesh.surface_set_material(0, sm)
 
 @export var update: bool = false
@@ -93,8 +104,8 @@ func _process(delta):
 		update = false
 	pass
 
-func set_lod(_lod:int):
-	lod = _lod
+func set_resolution(_resolution:int):
+	resolution = _resolution
 
 func get_center():
 	return center_terrain
