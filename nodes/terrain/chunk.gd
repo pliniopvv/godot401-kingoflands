53885ed5f9444f7d52b008cd05dde79c82f605e7
gangss <gangss2@hotmail.com>
2023-09-25 23:29:07 -0300

    build_biomes funcionando.

--- nodes/terrain/chunk.gd
+++ nodes/terrain/chunk.gd
@@ -3,8 +3,8 @@ class_name GenericChunk
 
 @export_category("Configurações iniciais")
 @export_group("Texturas")
-@export var grama: Texture2D = preload("res://assets/texture/grass2.jpg")
-@export var deserto: Texture2D = preload("res://assets/texture/sand.jpg")
+@export var tGrama: Texture2D = preload("res://assets/texture/grass2.jpg")
+@export var tDeserto: Texture2D = preload("res://assets/texture/sand.jpg")
 @export var humidade: NoiseTexture2D
 @export_group("Valores iniciais")
 @export var size = Vector2(12.0,12.0)
@@ -14,24 +14,40 @@ class_name GenericChunk
 
 var center_terrain = size/2
 var center_offset = 0.5
+var lim_hum = 0.35
 
-var uvx = 0
-var uvz = 0
+#
+#		CONFIGURAÇÕES DOS BIOMAS
+#
+
+enum Bioma {
+	Floresta,
+	Deserto
+} 
+
+const grama_item := preload("res://assets/blender/grass.gltf")
+
 
 func _ready():
 	pass
 
 func generate_terrain(bnoise: FastNoiseLite, noise: FastNoiseLite, st:SurfaceTool, posOrigin:Vector2):
-	var mSize = Vector3(size.x, 1.0, size.y)
-	position = Vector3(posOrigin.x, 0, posOrigin.y) * mSize
+	randomize()
+	var sizev3 = Vector3(size.x, 1.0, size.y)
+	position = Vector3(posOrigin.x, 0, posOrigin.y) * sizev3
 	for _z in range(resolution+1):
 		for _x in range(resolution+1):
 			var percent = Vector2(_x,_z)/resolution
 			var pontoNaMesh = Vector3((percent.x-center_offset), 0, (percent.y-center_offset))
+			# inserindo grama
+			#for i in range(densidade_grama/resolution):
+				#var pgrass = (Vector3(randf()-center_offset,0,randf()-center_offset))
+				#pgrass.y = noise.get_noise_2d(posOrigin.x+pgrass.x, posOrigin.y+pgrass.z) * altura * resolution
+				#insert_grass(pgrass)
+			# ###############
 			if not flat:
 				pontoNaMesh.y = noise.get_noise_2d(posOrigin.x+pontoNaMesh.x, posOrigin.y+pontoNaMesh.z) * altura * resolution
-			var vertex = pontoNaMesh * mSize
-			
+			var vertex = pontoNaMesh * sizev3
 			var uv = Vector2(0,0)
 			uv.x = percent.x
 			uv.y = percent.y
@@ -67,11 +83,13 @@ func generate_terrain(bnoise: FastNoiseLite, noise: FastNoiseLite, st:SurfaceToo
 	#				CONFIGURANDO BIOMAS
 	#
 	
-	var oeste = grama
-	var leste = grama
-	var norte = grama
-	var sul = grama
-	var biome = grama
+	var type_biome = Bioma.Floresta
+	
+	var oeste = tGrama
+	var leste = tGrama
+	var norte = tGrama
+	var sul = tGrama
+	var bioma = tGrama
 	
 	var hum = bnoise.get_noise_2dv(posOrigin)
 	var hOeste = bnoise.get_noise_2d(posOrigin.x-1, posOrigin.y)
@@ -79,24 +97,27 @@ func generate_terrain(bnoise: FastNoiseLite, noise: FastNoiseLite, st:SurfaceToo
 	var hNorte = bnoise.get_noise_2d(posOrigin.x, posOrigin.y+1)
 	var hSul = bnoise.get_noise_2d(posOrigin.x, posOrigin.y-1)
 	
-	if (hum < 0.35):
-		biome = deserto
-	if (hOeste < 0.35):
-		oeste = deserto
-	if (hLeste < 0.35):
-		leste = deserto
-	if (hNorte < 0.35):
-		norte = deserto
-	if (hSul < 0.35):
-		sul = deserto
+	if (hum < lim_hum):
+		bioma = tDeserto
+		type_biome = Bioma.Deserto
+	if (hOeste < lim_hum):
+		oeste = tDeserto
+	if (hLeste < lim_hum):
+		leste = tDeserto
+	if (hNorte < lim_hum):
+		norte = tDeserto
+	if (hSul < lim_hum):
+		sul = tDeserto
 	
-	sm.set_shader_parameter("biome", biome)
+	sm.set_shader_parameter("bioma", bioma)
 	sm.set_shader_parameter("oeste", oeste)
 	sm.set_shader_parameter("leste", leste)
 	sm.set_shader_parameter("norte", norte)
 	sm.set_shader_parameter("sul", sul)
 	
 	mesh.surface_set_material(0, sm)
+	
+	build_biome(noise, type_biome)
 
 @export var update: bool = false
 func _process(delta):
@@ -109,3 +130,26 @@ func set_resolution(_resolution:int):
 
 func get_center():
 	return center_terrain
+
+@export_group("Configurações do bioma de grama")
+@export var densidade_grama = 100
+@export var visible_grama = true
+func build_biome(noise: FastNoiseLite, type_biome: Bioma):
+	match type_biome:
+		Bioma.Deserto:
+			pass
+		_:
+			for i in range(densidade_grama):
+				randomize()
+				var pos = Vector3(randf()-center_offset,0,randf()-center_offset)
+				pos.y = noise.get_noise_2d(position.x/size.x+pos.x,position.z/size.y+pos.z) * altura * resolution
+				insert_grass(pos)
+			pass
+
+func insert_grass(pos: Vector3):
+	var sizev3 = Vector3(size.x,1,size.y)
+	var item = grama_item.instantiate()
+	item.position = pos*sizev3
+	item.scale = Vector3(0.3,0.3,0.3)
+	item.visible = visible_grama
+	add_child(item)
